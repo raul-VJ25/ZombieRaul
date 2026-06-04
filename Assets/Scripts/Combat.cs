aee7f24e125221d680321f9d95ef00b950afcdd9
raul-VJ25 <1881623@alu.murciaeduca.es>
2026-06-05 00:49:53 +0200

    parte17terminada

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Combat : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class Combat : MonoBehaviour
     [SerializeField] private float attackRadius = 0.6f;
     [SerializeField] private Vector2 dmgRange = new Vector2(2f, 5f);
 
+    private Animator anim;
+    private NavMeshAgent agent;
+    private Camera cam;
+
     private void Awake()
     {
         isPlayer = gameObject.CompareTag("Player");
@@ -16,10 +21,39 @@ public class Combat : MonoBehaviour
         if (isPlayer)
         {
             opponentMask = LayerMask.GetMask("Enemy");
+
+            anim = GetComponent<Animator>();
+            agent = GetComponent<NavMeshAgent>();
+            cam = Camera.main;
         }
         else
         {
             opponentMask = LayerMask.GetMask("Player");
+        }
+    }
+
+    private void Update()
+    {
+        if (!isPlayer) return;
+        anim.SetBool("Attack", Input.GetMouseButton(1));
+
+
+        PlayerLookAtEnemy();
+    }
+
+    private void PlayerLookAtEnemy()
+    {
+        if (Input.GetMouseButton(1) && agent.velocity.magnitude < 0.05f)
+        {
+            Vector3 input = Input.mousePosition;
+
+            Vector3 lookPoint = cam.ScreenToWorldPoint(new Vector3(input.x, input.y, 10f));
+
+            lookPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+
+            Quaternion lookRot = Quaternion.LookRotation(lookPoint - transform.position);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRot, Time.deltaTime * 10f);
         }
     }
 
